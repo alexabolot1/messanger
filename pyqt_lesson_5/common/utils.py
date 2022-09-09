a3f1c6c2796408c038a8c5a56392da6dcbc3ce26
alex <alexabolot@gmail.com>
2022-09-09 13:04:03 +0500

    Выполнение домашнего задания к пятому уроку

--- pyqt_lesson_5/common/utils.py
+++ pyqt_lesson_5/common/utils.py
@@ -1,40 +1,27 @@
-"""Утилиты"""
 import json
-import os
 import sys
-sys.path.insert(0, os.path.join(os.getcwd(), '..'))
-from common.variables import MAX_PACKAGE_LENGTH, ENCODING
-from decos import log
+sys.path.append('../')
+from common.decos import log
+from common.variables import *
 
+
+# Утилита приёма и декодирования сообщения
+# принимает байты выдаёт словарь, если приняточто-то другое отдаёт ошибку типа
 @log
 def get_message(client):
-    """
-    Утилита приёма и декодирования сообщения.
-    Принимает байты, выдаёт словарь, если принято что-то
-    другое возвращает ValueError (ошибку значения)
-    """
-
     encoded_response = client.recv(MAX_PACKAGE_LENGTH)
-    if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode(ENCODING)
-        if isinstance(json_response, str):
-            response = json.loads(json_response)
-            if isinstance(response, dict):
-                return response
-            raise ValueError
-        raise ValueError
-    raise ValueError
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
 
 
+# Утилита кодирования и отправки сообщения
+# принимает словарь и отправляет его
 @log
 def send_message(sock, message):
-    """
-    Утилита кодирования и отправки сообщения:
-    принимает для отправки словарь, получает из него строку,
-    далее превращает строку в байты и отправляет.
-    """
-    if not isinstance(message, dict):
-        raise TypeError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
